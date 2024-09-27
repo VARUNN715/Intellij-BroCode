c0d0efe69408a7c58958902755aa16a2fb58f287
VARUNN715 <varunn390@gmail.com>
2024-09-27 23:12:31 +0530

    this is a commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+
+        int x; //declaration
+        x = 123; // assignment
+
+        int a = 900; // initialization
+        System.out.println(x);
+    }
+
+
+    }
